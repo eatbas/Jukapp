a7c084fbd34f5cb486783abff17e2f08c04d9578
bcaputcu <berk.caputcu@shopify.com>
2014-09-16 19:22:04 -0400

    cleanup in videos controller, creates search action

--- app/views/videos/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @video, :id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :videos
+  get "search" => 'videos#search'
+
+  root "videos#search"
 end
