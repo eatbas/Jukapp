e78323bab6aeb6ab87e8d3b83c20c6fcdb19703b
bcaputcu <berk.caputcu@shopify.com>
2014-09-25 11:05:29 -0400

    some validations for name

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,6 @@ class Room < ActiveRecord::Base
   has_many :videos, through: :queued_videos
 
   validates_uniqueness_of :name
+  validates_presence_of :name
+  validates_length_of :name, in: 3..30
 end
