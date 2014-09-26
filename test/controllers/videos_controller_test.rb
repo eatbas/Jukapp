679975737fd40d596b0d75d5137323f6414e3efd
bcaputcu <berk.caputcu@shopify.com>
2014-09-25 22:01:38 -0400

    tests for video events

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class VideosControllerTest < ActionController::TestCase
+  def setup
+    join_room rooms(:cafe)
+  end
+
   test "GET to :search without any params doesn't call YoutubeService" do
     YoutubeService.expects(:get_videos_for).never
     get :search
+    assert_response :ok
   end
 
   test "GET to :search with query calls YoutubeService" do
     YoutubeService.expects(:get_videos_for).with("my favorite video")
     get :search, query: "my favorite video"
+    assert_response :ok
   end
 end
--- test/models/video_event_test.rb
+++ test/models/video_event_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class VideoEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @room = rooms(:confederation)
+    @video = videos(:funny_video)
+  end
+
+  test "#play creates a new video event if video has never played in room" do
+    VideoEvent.find_by(video: @video, room: @room).destroy
+
+    assert_difference "VideoEvent.count" do
+      VideoEvent.play(@video, @room)
+    end
+
+    event = VideoEvent.last
+    assert_equal @video, event.video
+    assert_equal @room, event.room
+  end
+
+  test "#play does not create a new video event if video has played in room before" do
+    VideoEvent.play(@video, @room)
+    assert_no_difference "VideoEvent.count" do
+      VideoEvent.play(@video, @room)
+    end
+  end
+
+  test "#play increments the play count and sets played_at" do
+    event = VideoEvent.find_by(video: @video, room: @room)
+    assert_difference "event.reload.play_count" do
+      VideoEvent.play(@video, @room)
+    end
+  end
 end
