3108088fc2c323080ad13b4af96c52bf535dc638
bcaputcu <berk.caputcu@shopify.com>
2014-09-26 19:08:14 -0400

    adds username as attr_acessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 
+  attr_accessible :username
+
   def email_required?
     false
   end
