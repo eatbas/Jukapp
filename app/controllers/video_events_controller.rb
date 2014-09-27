59c47fce7332363b665f706b4e53fab501cca406
bcaputcu <berk.caputcu@shopify.com>
2014-09-27 13:38:09 -0400

    shows queued videos

--- app/controllers/video_events_controller.rb
+++ app/controllers/video_events_controller.rb
@@ -2,6 +2,7 @@ class VideoEventsController < ApplicationController
   before_action :ensure_in_room
 
   def index
+    @queued_videos   = QueuedVideo.includes(:video).queue_in(current_room).map(&:video)
     @top_ten_videos  = VideoEvent.includes(:video).top_ten_in(current_room).map(&:video)
     @last_ten_videos = VideoEvent.includes(:video).last_ten_in(current_room).map(&:video)
   end
