b457dc7845db2c95bd59c3e790e3562b3bd4ca4f
berkcaputcu <berk.caputcu@shopify.com>
2014-10-26 17:49:22 -0400

    remove dependency, yay!

--- app/services/youtube_service.rb
+++ app/services/youtube_service.rb
@@ -5,6 +5,6 @@ class YoutubeService
     query_with_music_category = { query: query, category: "music", max_results: 15 }
 
     client.search(query_with_music_category)
-    client.videos
+    client.videos.map { |video| OpenStruct.new(video) }
   end
 end
