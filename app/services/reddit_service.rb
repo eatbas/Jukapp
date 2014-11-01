03c7ddaab84430182aae63969123baae2b0ca304
berkcaputcu <berk.caputcu@shopify.com>
2014-11-01 14:13:31 -0400

    more random videos

--- app/services/reddit_service.rb
+++ app/services/reddit_service.rb
@@ -1,7 +1,22 @@
 class RedditService
   def self.get_video_from(subreddit)
-    client = RedditKit::Client.new("Jukapp", ENV["REDDIT_PASSWORD"])
-    youtube_url = client.links(subreddit, limit: 1).first.url
+    youtube_url = RedisService.next_link
+
+    unless youtube_url
+      links = youtube_links_from(subreddit)
+      RedisService.add(links[0..-2])
+      youtube_url = links.last
+    end
+
     YoutubeService.get_video_from_url(youtube_url)
+  end
+
+  private
+  def self.youtube_links_from(subreddit)
+    client.links(subreddit).map(&:url).select { |link| link =~ /youtu.?be/ }
+  end
+
+  def self.client
+    @client ||= RedditKit::Client.new("Jukapp", ENV["REDDIT_PASSWORD"])
   end
 end
