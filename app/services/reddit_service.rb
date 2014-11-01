660a55353da9a374add9303030b30bae52d11062
berkcaputcu <berk.caputcu@shopify.com>
2014-11-01 13:08:30 -0400

    get video from reddit, same video all the time

--- app/services/reddit_service.rb
+++ app/services/reddit_service.rb
@@ -1,7 +1,7 @@
 class RedditService
   def self.get_video_from(subreddit)
     client = RedditKit::Client.new("Jukapp", ENV["REDDIT_PASSWORD"])
-    client.links(subreddit)
-    byebug
+    youtube_url = client.links(subreddit, limit: 1).first.url
+    YoutubeService.get_video_from_url(youtube_url)
   end
 end
