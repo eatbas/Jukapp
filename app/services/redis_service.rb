904278f04f88b8a7426679cbad3dac574136a925
berkcaputcu <berk.caputcu@shopify.com>
2014-11-08 13:20:32 -0500

    fetch links and better youtube url tester

--- app/services/redis_service.rb
+++ app/services/redis_service.rb
@@ -5,16 +5,27 @@ class RedisService
   end
 
   def self.next_link
-    if unparsed_links = client.get("youtube_links")
-      links = JSON.parse(unparsed_links)
+    if links = fetch_links
       client.set("youtube_links", links[0..-2])
     end
 
     links.try(:last)
   end
 
+  def self.list_links
+    if links = fetch_links
+      links.each { |link| puts link }
+    end
+  end
+
   private
   def self.client
     @client ||= Redis.new(:url => ENV["REDISCLOUD_URL"])
+  end
+
+  def self.fetch_links
+    if unparsed_links = client.get("youtube_links")
+      links = JSON.parse(unparsed_links)
+    end
   end
 end
