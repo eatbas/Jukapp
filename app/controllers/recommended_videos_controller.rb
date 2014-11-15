3c72854c6a11bb75491cd4c4289dcb3980c400d9
berkcaputcu <berk.caputcu@shopify.com>
2014-11-15 11:09:47 -0500

    queue in pinned videos

--- app/controllers/recommended_videos_controller.rb
+++ app/controllers/recommended_videos_controller.rb
@@ -2,6 +2,7 @@ class RecommendedVideosController < ApplicationController
   before_action :ensure_in_room
 
   def index
+    @queued_videos      = QueuedVideo.queue_in(current_room)
     @recommendedations  = RecommendedVideo.where(room: current_room).page(params[:page]).includes(:video)
     @recommended_videos = @recommendedations.map(&:video)
   end
