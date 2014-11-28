eaba9fa316c123337f91dc7d124956de8c3c74fa
berkcaputcu <berk.caputcu@shopify.com>
2014-11-27 19:49:26 -0500

    better sorting

--- app/controllers/video_events_controller.rb
+++ app/controllers/video_events_controller.rb
@@ -8,8 +8,8 @@ class VideoEventsController < ApplicationController
   end
 
   def stats
-    order_column = params[:sort] == "played_at" ? "played_at" : "play_count"
+    order_column = params[:sort] == "played_at" ? "played_at DESC" : "play_count DESC, played_at DESC"
     @queued_videos   = QueuedVideo.queue_in(current_room)
-    @stats = VideoEvent.includes(:video).order("#{order_column} DESC").paginate(:page => params[:page])
+    @stats = VideoEvent.includes(:video).order("#{order_column}").paginate(:page => params[:page])
   end
 end
