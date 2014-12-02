12a52c720b5977119ae42b33ed4d1632ad4e64b6
berkcaputcu <berk.caputcu@shopify.com>
2014-12-02 18:50:42 -0500

    try this

--- db/migrate/20141122210006_add_owner_to_rooms.rb
+++ db/migrate/20141122210006_add_owner_to_rooms.rb
@@ -1,5 +1,5 @@
 class AddOwnerToRooms < ActiveRecord::Migration
   def change
-    add_reference :rooms, :owner, index: true, null: false, default: User.first.id
+    add_reference :rooms, :owner, index: true, null: false, default: User.first.try(:id)
   end
 end
