2a261097306438f7c9e09e0ba499d74bc776c2d2
berkcaputcu <berk.caputcu@shopify.com>
2014-12-10 09:30:57 -0500

    change how secret managed in pusher

--- app/services/event_stream_service.rb
+++ app/services/event_stream_service.rb
@@ -1,6 +1,6 @@
 class EventStreamService
   def self.send_message_to(room, message)
-    Pusher.url = "http://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_ID']}"
+    Pusher.url = ENV['PUSHER_URL']
     Pusher["queue-#{room.id}"].trigger(message, {})
   end
 end
