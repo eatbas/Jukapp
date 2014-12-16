4ab11d50fc7a065522a5410283a9a6bd11f3ae6e
berkcaputcu <berk.caputcu@shopify.com>
2014-12-16 14:10:39 -0500

    fix annoying bug

--- app/controllers/video_events_controller.rb
+++ app/controllers/video_events_controller.rb
@@ -4,6 +4,6 @@ class VideoEventsController < ApplicationController
   def index
     order_column = params[:sort] == "played_at" ? "played_at DESC" : "play_count DESC, played_at DESC"
     @queued_videos   = QueuedVideo.queue_in(current_room)
-    @stats = VideoEvent.includes(:video).order("#{order_column}").paginate(:page => params[:page])
+    @stats = VideoEvent.where(room: current_room).includes(:video).order("#{order_column}").paginate(:page => params[:page])
   end
 end
