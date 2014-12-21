06902b37df4ff63e982784e67cb05b58bffa5d7d
berkcaputcu <berk.caputcu@shopify.com>
2014-12-20 22:55:33 -0500

    populars -> popular

--- app/controllers/video_events_controller.rb
+++ app/controllers/video_events_controller.rb
@@ -3,7 +3,7 @@ class VideoEventsController < ApplicationController
 
   def index
     @recents   = VideoEvent.where(room: current_room).includes(:video).order("played_at DESC").paginate(:page => params[:page])
-    @populars  = VideoEvent.where(room: current_room).includes(:video).order("play_count DESC").paginate(:page => params[:page])
+    @popular  = VideoEvent.where(room: current_room).includes(:video).order("play_count DESC").paginate(:page => params[:page])
     @favorites = current_user ? Favorite.where(user: current_user).includes(:video).map(&:video) : []
   end
 end
