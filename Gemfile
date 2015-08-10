5881e28bb55f4a83d1c7237ac0870e9704f3093d
berkcaputcu <berk.caputcu@shopify.com>
2015-08-10 19:14:50 -0400

    some gem bumps and devise controllers

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.1.5'
 
-gem 'rails', '4.1.1'
+gem 'rails', '4.1.11'
 
 group :production do
   gem 'pg'
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,3 @@
 class Users::SessionsController < Devise::SessionsController
-  protect_from_forgery with: :null_session, except: :create
   respond_to :html, :json
-
-  def create
-    super
-  end
 end
